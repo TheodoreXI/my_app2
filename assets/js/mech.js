4ff65e9fa494b97a2b139d4b01374a37e0d327e8
alijr2018 <alijr_2018@proton.me>
2026-04-18 18:18:07 +0100

    login page and signup page are ready , linked to the backend just by making a server or changing the API to a non local one

---
+++ assets/js/mech.js
@@ -0,0 +1,27 @@
+const API_URL = "http://loclahost:3000"; // this gonna change to your backend api 
+
+export async function signup(email, password, userData)
+{
+	const res = await fetch(`${API_URL}/signup`, { 
+		method : "POST",
+		headers: {"Content-Type": "application/json"},
+		body: JSON.stringify({email, password, userData})
+	});
+	
+	if (!res.ok)
+		throw Error("Signup failed");
+	return res.text();
+}
+
+export async function login(email, password)
+{
+	const res = await fetch(`${API_URL}/login`, { 
+		method : "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password})
+        });
+
+        if (!res.ok)
+                throw Error("login failed");
+        return res.text();
+}
